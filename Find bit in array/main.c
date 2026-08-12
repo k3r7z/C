89b892744001f5070fb6a144e857dcb1940ffb80
k3r7z <k3r7z@protonmail.com>
2026-08-12 10:59:45 -0300

    Finds the bit equals to 1 in an array of N bits using threads

---
+++ Find bit in array/main.c
@@ -0,0 +1,96 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+
+typedef struct{
+    int thread_number;
+    bool *array;
+    int from, to;
+    bool *found;
+} ThreadData;
+
+
+void *find_bit(void *arg){
+    ThreadData *data = (ThreadData*) arg;
+
+    int i = data->from;
+    while( *data->found == false && i < data->to ){
+	if( data->array[i] == true ){
+	    printf("[Thread %d] Bit found in the position %d\n", data->thread_number, i);
+	    *data->found = true;
+	    return NULL;
+	}
+	++i;
+    }
+
+    if( *data->found == true)
+	printf("[Thread %d] Stopping because bit was found in other thread\n", data->thread_number);
+    else
+	printf("[Thread %d] Couldn't find the bit in the assignated range\n", data->thread_number);
+    
+    return NULL;
+}
+
+
+static void print_help(){
+    printf("-n <number of threads> (default 4) -s <size of the array> (default 1 000 000)\n");
+}
+
+int main(int argc, char **argv){
+    int opt;
+    int array_size = 1000000;
+    int number_of_threads = 4;
+
+    while( (opt = getopt(argc, argv, "n:s:h")) != -1){
+	switch(opt){
+	case 'h':
+	    print_help();
+	    return 0;
+	case 'n':
+	    number_of_threads = atoi(optarg);
+	    break;
+	case 's':
+	    array_size = atoi(optarg);
+	    break;
+	case '?':
+	    return 1;
+	}
+    }
+    
+    time_t start = clock();
+    srand(time(NULL));
+    bool *array = malloc(array_size * sizeof(bool));
+    for(int i = 0; i < array_size; ++i)
+	array[i] = false;
+    int random_position = rand() % array_size;
+    array[random_position] = true;
+    printf("The 1 is on the %d position\n", random_position);
+
+    bool *found = malloc(sizeof(bool));
+    int range_division = array_size / number_of_threads;
+    pthread_t threads[number_of_threads];
+    ThreadData data[number_of_threads];
+    for(int i = 0; i < number_of_threads; ++i){
+	data[i].thread_number = i + 1;
+	data[i].array = array;
+	data[i].from = range_division * i;
+	data[i].to = range_division * (i + 1);
+	data[i].found = found;
+	pthread_create(&threads[i], NULL, find_bit, &data[i]);
+    }
+
+    for(int i = 0; i < number_of_threads; ++i )
+	pthread_join(threads[i], NULL);
+
+    free(found);
+    free(array);
+    array = NULL;
+
+    double execution_time = (double) ( clock() - start ) / CLOCKS_PER_SEC;
+    printf("Execution time in %.3f seconds\n", execution_time);
+    return 0;
+}
